23840c2964583761f34befb0ead5952d460e15be
jD91mZM2 <me@krake.one>
2018-01-03 17:53:05 +0100

    Revert "Internally tagged packets"
    This reverts commit 558400165f45ef14b8e848b4e29f0c19766ba020.

--- src/lib.rs
+++ src/lib.rs
@@ -179,7 +179,7 @@ pub struct UserReceive {
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
-#[serde(rename_all = "snake_case", tag = "type")]
+#[serde(rename_all = "snake_case")]
 pub enum Packet {
     /// a polite way to close, since the server otherwise won't notice you're gone until some time
     Close,
